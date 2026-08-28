a71c3328b935e12302714e09328f8f7a09a063bf
alanabarbosaugc-spec <alanabarbosaugc@gmail.com>
2026-08-27 21:12:57 -0300

    Adiciona link do TikTok

--- verificar.js
+++ verificar.js
@@ -30,7 +30,7 @@ setInterval(async () => {
         embeds: [
           {
             title: "🔴 Ohandle está AO VIVO!",
-            description: "Venha acompanhar a live agora!",
+            description: "Venha acompanhar a live agora!\n\n🎥 https://www.tiktok.com/@ohandle_",
             color: 16711680,
             image: {
               url: "https://media.discordapp.net/attachments/1520107551708942512/1541945882272993290/Notificacao_de_live.jpeg?ex=6a8f7082&is=6a8e1f02&hm=7c2dcf2bd02cf16a722cdfe4937a3cd8071ad30a3ac8f25504ec70f94934c4da&=&format=webp&width=1024&height=1024"
@@ -51,4 +51,4 @@ setInterval(async () => {
 
   }
 
-}, 10000);
+}, 60000);
