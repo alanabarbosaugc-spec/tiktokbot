92d852f12ba8f1601960d272bbd36142f5c7c64b
alanabarbosaugc-spec <alanabarbosaugc@gmail.com>
2026-08-30 18:39:14 -0300

    Melhora deteccao da live

--- verificar.js
+++ verificar.js
@@ -1,14 +1,23 @@
 require("dotenv").config();
+
 const { TikTokLiveConnection } = require("tiktok-live-connector");
 const axios = require("axios");
 
 const webhook = process.env.WEBHOOK;
 
+if (!webhook) {
+  console.error("WEBHOOK não configurado!");
+  process.exit(1);
+}
+
 let ultimaNotificacao = 0;
+let deteccoes = 0;
 
 console.log("Monitor iniciado...");
 
 setInterval(async () => {
+
+  console.log("Verificando live...");
 
   const tiktokLive = new TikTokLiveConnection(
     "ohandle_",
@@ -19,11 +28,16 @@ setInterval(async () => {
 
     await tiktokLive.connect();
 
-    console.log("LIVE ENCONTRADA!");
+    deteccoes++;
+
+    console.log(`LIVE ENCONTRADA! (${deteccoes})`);
 
     const agora = Date.now();
 
-if (agora - ultimaNotificacao > 4 * 60 * 60 * 1000) {
+    if (
+      deteccoes >= 2 &&
+      agora - ultimaNotificacao > 4 * 60 * 60 * 1000
+    ) {
 
       ultimaNotificacao = agora;
 
@@ -32,24 +46,23 @@ if (agora - ultimaNotificacao > 4 * 60 * 60 * 1000) {
         embeds: [
           {
             title: "🔴 Ohandle está AO VIVO!",
-            description: "Venha acompanhar a live agora!\n\n🎥 https://www.tiktok.com/@ohandle_",
+            description: "🔥 A live acabou de começar!\n\n🎥 Assista agora:\nhttps://www.tiktok.com/@ohandle_",
             color: 16711680,
             image: {
-              url: "https://media.discordapp.net/attachments/1520107551708942512/1541945882272993290/Notificacao_de_live.jpeg?ex=6a8f7082&is=6a8e1f02&hm=7c2dcf2bd02cf16a722cdfe4937a3cd8071ad30a3ac8f25504ec70f94934c4da&=&format=webp&width=1024&height=1024"
+              url: "https://media.discordapp.net/attachments/1520107551708942512/1541945882272993290/Notificacao_de_live.jpeg?ex=6a8f7082&is=6a8e1f02&hm=7c2dcf2bd02cf16a722cdfe4937a3cd8071ad30a3ac8f25504ec70f94934c4da&format=webp&width=1024&height=1024"
             }
           }
         ]
       });
 
       console.log("Notificação enviada!");
-
     }
 
   } catch {
 
+    deteccoes = 0;
+
     console.log("Offline");
-
-
   }
 
 }, 60000);
